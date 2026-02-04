9a5a1e03dd4400ae1bc4e26305a904f4c3847e55
emateo <ematgim@gmail.com>
2026-02-04 14:32:58 +0100

    feat: add Jest setup file to suppress console output during tests

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+// Global Jest setup to suppress console output during tests
+global.console = {
+  ...console,
+  log: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
+};
